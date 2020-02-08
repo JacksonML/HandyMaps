e001858bf1199e244a5c4218841a978e3ec0ab30
JacksonML <jml10649@uga.edu>
2020-02-07 21:02:30 -0500

    Initial Commit

---
+++ HandyMaps/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HandyMaps
+//
+//  Created by Jackson Lucas on 2/7/20.
+//  Copyright © 2020 HandyMaps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
